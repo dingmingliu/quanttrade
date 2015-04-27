2d70d73bdd4b07ea347180bdd6a78a5bf16f040f
dingmingliu <dingming168@gmail.com>
2015-04-27 19:12:00 +0800

    spider

---
+++ quantspider/quantspider/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for quantspider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'quantspider'
+
+SPIDER_MODULES = ['quantspider.spiders']
+NEWSPIDER_MODULE = 'quantspider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'quantspider (+http://www.yourdomain.com)'
